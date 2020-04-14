049dad89c84bdd5338460980899ca640bd057893
onifademola <onifademola@gmail.com>
2020-04-14 20:31:10 +0100

    fix v1 to request header

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cors = require('cors');
 const express = require('express');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const xmlparser = require('express-xml-bodyparser');
 
 //requires
@@ -20,8 +20,9 @@ app.use(cors({
 }));
 //app.use(bodyParser.json());
 //app.use(bodyParser.json());
-app.use(express.text());
-app.use(express.json());
+app.use(express.text({ type: 'text/plain' }));
+app.use(express.json({ type: 'application/json' }));
+//app.use(express.json({ type: 'application/xml' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(xmlparser());
 app.use((req, res, next) => {
